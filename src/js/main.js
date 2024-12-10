d3419371cdce2c5c541722b367734ef4ea718060
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2024-12-11 00:47:21 +0100

    nav,header,about us, hero, offers sections finished. mobile and desktop versions work correctly

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
-const hej = () => {
-	console.log("hej")
+const openBurger = document.querySelector(".fa-bars")
+const closeBurger = document.querySelector(".fa-x")
+const burgerBtn = document.querySelector(".nav__burger")
+const navLinks = document.querySelectorAll(".nav__link")
+const boxLinks = document.querySelector(".nav__box")
+
+const handleBurger = () => {
+	openBurger.classList.toggle("hide")
+	closeBurger.classList.toggle("hide")
+		boxLinks.classList.toggle("active")
 }
 
-hej()
+burgerBtn.addEventListener("click", handleBurger)
+
+navLinks.forEach(link => link.addEventListener("click", handleBurger))
