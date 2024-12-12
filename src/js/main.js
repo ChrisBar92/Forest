e7780e6f096b8d004fda57e6d1484a869679090b
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2024-12-12 17:12:00 +0100

    footer for desktop and mobile finished. details in sections changed

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,20 @@ const closeBurger = document.querySelector(".fa-x")
 const burgerBtn = document.querySelector(".nav__burger")
 const navLinks = document.querySelectorAll(".nav__link")
 const boxLinks = document.querySelector(".nav__box")
+const footerYear = document.querySelector(".footer__year")
 
 const handleBurger = () => {
 	openBurger.classList.toggle("hide")
 	closeBurger.classList.toggle("hide")
-		boxLinks.classList.toggle("active")
+	boxLinks.classList.toggle("active")
 }
+
+const handleYear = () => {
+	const currentYear = new Date()
+	footerYear.textContent = currentYear.getFullYear()
+}
+
+handleYear()
 
 burgerBtn.addEventListener("click", handleBurger)
 
