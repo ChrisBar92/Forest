0052bd4060826a3114fa6ae8c4830401d012993c
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2024-12-21 15:34:11 +0100

    section contact finished for mobile and desktop, scrollspy finished and some details more also finished

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,46 @@ const burgerBtn = document.querySelector(".nav__burger")
 const navLinks = document.querySelectorAll(".nav__link")
 const boxLinks = document.querySelector(".nav__box")
 const footerYear = document.querySelector(".footer__year")
+const submitBtn = document.querySelector(".contact__btn")
+const allInputs = document.querySelectorAll(".contact__input")
+const textarea = document.querySelector(".contact__textarea")
+const allSections = document.querySelectorAll(".section")
+let id = ""
+
+document.addEventListener("DOMContentLoaded", () => {
+	const observer = new IntersectionObserver(
+		entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					id = entry.target.id
+					navLinks.forEach(link => {
+						link.classList.remove("active")
+						boxLinks.querySelector(`[data-section="${id}"]`).classList.add("active")
+					})
+				}
+			})
+		},
+		{ rootMargin: "-20% 0px -40% 0px" }
+	)
+	allSections.forEach(section => {
+		observer.observe(section)
+	})
+})
 
 const handleBurger = () => {
 	openBurger.classList.toggle("hide")
 	closeBurger.classList.toggle("hide")
-	boxLinks.classList.toggle("active")
+	boxLinks.classList.toggle("show-nav")
+	boxLinks.classList.contains("show-nav")
+		? document.body.classList.add("block-scrolling")
+		: document.body.classList.remove("block-scrolling")
+}
+
+const handleLinks = () => {
+	openBurger.classList.toggle("hide")
+	closeBurger.classList.toggle("hide")
+	boxLinks.classList.remove("show-nav")
+	document.body.classList.remove("block-scrolling")
 }
 
 const handleCurrentYear = () => {
@@ -16,8 +51,18 @@ const handleCurrentYear = () => {
 	footerYear.textContent = currentYear
 }
 
+const sendForm = e => {
+	e.preventDefault()
+	allInputs.forEach(input => (input.value = ""))
+	textarea.value = ""
+}
+
 handleCurrentYear()
 
 burgerBtn.addEventListener("click", handleBurger)
+navLinks.forEach(link => link.addEventListener("click", handleLinks))
 
-navLinks.forEach(link => link.addEventListener("click", handleBurger))
+if (document.body.classList.contains("contact-page")) {
+	boxLinks.querySelector(`[data-section="contact"]`).classList.add("active")
+	submitBtn.addEventListener("click", sendForm)
+}
